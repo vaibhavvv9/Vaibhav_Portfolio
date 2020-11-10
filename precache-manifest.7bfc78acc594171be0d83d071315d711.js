6e3fefea0845b512d509f66891817f388b6d29a8
vaibhavvv9 <vaibhavgood9@gmail.com>
2020-11-11 00:48:57 +0530

    Updates

---
+++ precache-manifest.7bfc78acc594171be0d83d071315d711.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817975a4b96ea125dce79dc99480fddd",
+    "url": "/Vaibhav_Portfolio/index.html"
+  },
+  {
+    "revision": "43a9faab85e2c802c09e",
+    "url": "/Vaibhav_Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/Vaibhav_Portfolio/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Vaibhav_Portfolio/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a9faab85e2c802c09e",
+    "url": "/Vaibhav_Portfolio/static/js/main.453a8042.chunk.js"
+  },
+  {
+    "revision": "0bff64f002910e527975",
+    "url": "/Vaibhav_Portfolio/static/js/runtime-main.6c1819ee.js"
+  }
+]);
